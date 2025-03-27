11b58751a9f14b47071d024a321f4756042a68ae
Codeminister2137 <kubajaworski99@wp.pl>
2025-03-27 18:34:23 +0100

    Updating pre-commit config

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,49 @@
-from calendar_app.models import Calendar, Category, Task
+from datetime import datetime, timedelta
+
+import pytest
+
+from calendar_app.models import Calendar, Category, Status, Task
+
+
+@pytest.fixture
+def sample_task():
+    return Task(
+        name="Sample Task",
+        expected_duration=timedelta(hours=2),
+        actual_duration=timedelta(hours=1),
+        categories=[Category(name="Work"), Category(name="Urgent")],
+        deadline=datetime(2025, 3, 30),
+        priority=3,
+        notifications=[datetime(2025, 3, 28), datetime(2025, 3, 29)],
+        status=Status.PENDING,
+    )
+
 
 example_tasks = [
-    Task(name="homework", categories=[Category(name="boring"), Category(name="short")]),
-    Task(name="dogwalk", categories=[Category(name="fun"), Category(name="short")]),
-    Task(name="project meeting", categories=[Category(name="meeting")]),
+    Task(
+        name="homework",
+        expected_duration=timedelta(hours=1),
+        deadline=datetime(2025, 4, 25),
+        priority=2,
+        status=Status.INIT,
+        categories=[Category(name="boring"), Category(name="short")],
+    ),
+    Task(
+        name="dogwalk",
+        expected_duration=timedelta(hours=2),
+        deadline=datetime(2025, 3, 30),
+        priority=3,
+        status=Status.PENDING,
+        categories=[Category(name="fun"), Category(name="short")],
+    ),
+    Task(
+        name="project meeting",
+        expected_duration=timedelta(hours=3),
+        deadline=datetime(2025, 3, 27),
+        priority=1,
+        status=Status.FINISHED,
+        categories=[Category(name="meeting")],
+    ),
 ]
 
 
